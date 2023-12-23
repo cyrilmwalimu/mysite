6737323a814be8b6ea7ab5858f872dc342709252
= <=>
2023-12-23 13:29:21 +0300

    changed nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-
+import {AiOutlineClose,AiOutlineMenu} from 'react-icons/ai'
+import {useState} from 'react'
 
 function App() {
+  
  
 
   return (
     <>
-    <p>hello</p>
-    <p>hello here</p>
+   <nav>
+    <div className="logo">Cyril Mwalimu</div>
+    <ul className="nav-links">
+      <li><a href="#about">About</a></li>
+      <li><a href="#experience">Experience</a></li>
+      <li><a href="#projects">Projects</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </ul>
+
+   </nav>
     </>
   )
 }
